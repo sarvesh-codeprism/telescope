bec45fad817e037350724183c238f00b1ac42ddc
Sarvesh <sarveshspatil111@gmail.com>
2022-01-16 11:29:02 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { Routes } from "./components/Routes";
 
-function App() {
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="{darkTheme ? 'dark' : ''}">
+    <div className="bg-gray-100">
+      Telescope
+    </div>
     </div>
   );
-}
+};
 
 export default App;
